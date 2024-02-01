dc6d6d5c0f226aee9279b2720ab4137f57ae1056
savidyainduma <87478305+savidyainduma@users.noreply.github.com>
2024-02-02 02:07:33 +0530

    changed some styles in admin login page

--- frontend/src/components/AdminLogin.js
+++ frontend/src/components/AdminLogin.js
@@ -11,18 +11,18 @@ const AdminLogin = () => {
         <div className='flex flex-col gap-8 justify-center bg-[#212125] px-10 py-3 rounded-3xl h-[550px] w-[480px] '>
                 <div className='flex items-center gap-2'>
                     <img src={Logo} className='w-[72px]'/>
-                    <div className=' font-medium flex flex-col gap-1'><h2 className='bodyFont text-[40px]'>Admin Login</h2><p className= ' text-[#a9a9a9]'>Doesn't have an account yet? <u className='underline-offset-4'>Sign up</u></p></div>
+                    <div className=' font-medium flex flex-col'><h2 className='bodyFont text-[40px]'>Admin Login</h2><p className= ' text-[#a9a9a9]'>Doesn't have an account yet? <u className='underline-offset-[6px] cursor-pointer'>Sign up</u></p></div>
                 </div>
                 <div className='flex items-start flex-col gap-1 '>
                     <p>Username</p>
-                    <input type='text' className='bg-black py-3 w-full rounded-md'/>
+                    <input type='text' className='bg-black px-4 py-3 w-full rounded-md outline-none'/>
                 </div>
                 <div className='flex items-start flex-col gap-1 '>
                 <p>Password</p>
-                    <input type='password' className='bg-black py-3 w-full rounded-md'/>
+                    <input type='password' className='bg-black py-3 w-full rounded-md px-4 outline-none'/>
                 </div>
-                <div className='flex gap-2 items-center'><input type='checkbox' className='accent-black' /><p>Remember me</p></div>
-                <div><button className='w-full flex items-center justify-center py-3 bg-[#ff2d2e] rounded-md'>Login</button></div>
+                <div className='flex gap-2 items-center'><input type='checkbox' className='accent-black bg-black' /><p>Remember me</p></div>
+                <div><button className='w-full flex items-center justify-center py-3 bg-[#ff2d2e] active:bg-[rgb(255,30,30)] rounded-md'>Login</button></div>
         </div>
     </div>
   )
